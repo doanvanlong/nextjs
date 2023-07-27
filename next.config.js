b7071d8eefc9d917bcf6bc8474a14cb44c52edb6
Đoàn Văn Long <47972513+doanvanlong@users.noreply.github.com>
2023-07-27 09:46:30 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/:slug',
-        has: [
-          {
-            type: 'header',
-            key: 'referer',
-            value: 'https://*.facebook.com/',
-          },
-        ],
-        permanent: true,
-       // destination: 'https://orgvn.top/:slug',
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/:slug',
+  //       has: [
+  //         {
+  //           type: 'header',
+  //           key: 'referer',
+  //           value: 'https://*.facebook.com/',
+  //         },
+  //       ],
+  //       permanent: true,
+  //      // destination: 'https://orgvn.top/:slug',
+  //     },
+  //   ];
+  // },
 }
